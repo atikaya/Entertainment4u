95509891793a6c5f85e2fa02dcd88298c22021ff
atikaya <dheerajsharma23949@gmail.com>
2026-07-15 11:41:30 +0530

    Update info.py enhance

--- info.py
+++ info.py
@@ -2,7 +2,6 @@ import re
 from os import environ
 
 id_pattern = re.compile(r'^.\d+$')
-
 def is_enabled(value, default):
     if value.lower() in ["true", "yes", "1", "enable", "y"]:
         return True
@@ -15,21 +14,18 @@ def is_enabled(value, default):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = 31343770
 API_HASH = "2ed4a8bfcd6e4a3bc650278a162b625e"
-BOT_TOKEN = "6399140480:AAFeCQxpOxBfa4sUr9ji3XEGQEJtXNKRCiw"
+BOT_TOKEN = "6399140480:AAFeCQxpOxBfa4sUr9ji3XEGQEJtXNKRciw"
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
-PICS = (environ.get(
-    'PICS',
-    'https://telegra.ph/file/b0d44589e96a84ae3123b.jpg https://telegra.ph/file/b0d44589e96a84ae3123b.jpg'
-)).split()
+PICS = (environ.get('PICS', 'https://telegra.ph/file/b0d44589e96a84ae3123b.jpg https://telegra.ph/file/b0d44589e96a84ae3123b.jpg')).split()
 
 # Admins, Channels & Users
 ADMINS = [5124873527]
-CHANNELS = [1811444060]
+CHANNELS = [-1811444060]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
-AUTH_USERS = (auth_users + ADMINS) if auth_users else []
+AUTH_USERS = (auth_users + ADMINS) if auth_users else ADMINS
 auth_channel = environ.get('AUTH_CHANNEL')
 auth_grp = environ.get('AUTH_GROUP')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
@@ -37,21 +33,30 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
 DATABASE_URI = "mongodb+srv://dheerajsharma23949_db_user:P3kaHF7FnOaSt10@accepttheself.ygag34g.mongodb.net/?appName=Accepttheself"
-DATABASE_NAME = "dheerajsharma23949_db_user"
-COLLECTION_NAME = "Telegram_files"
+DATABASE_NAME = "Accepttheself"
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-LOG_CHANNEL = 3494470609
+LOG_CHANNEL = -3494470609
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'TeamEvamaria')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
 SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "False")), False)
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", None)
-IMDB_TEMPLATE = environ.get(
-    "IMDB_TEMPLATE",
-    "<b>Query: {query}</b> \n‌IMDb Data:\n\n🏷 Title: <a href={url}>{title}</a>\n🎭 Genres: {genres}\n📆 Year: <a href={url}/releaseinfo>{year}</a>\n🌟 Rating: <a href={url}/ratings>{rating}</a> / 10"
-)
+IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", "<b>Query: {query}</b> \n‌IMDb Data:\n\n🏷 Title: <a href={url}>{title}</a>\n🎭 Genres: {genres}\n📆 Year: <a href={url}/releaseinfo>{year}</a>\n🌟 Rating: <a href={url}/ratings>{rating}</a> / 10")
 LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False"), False)
 SPELL_CHECK_REPLY = is_enabled(environ.get("SPELL_CHECK_REPLY", "True"), True)
 MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
-INDEX_REQ_CHANNEL = LOG_CHANNEL
+
+# Fixed a minor bug from your original code where passing the literal string 'LOG_CHANNEL' to int() would cause a crash.
+INDEX_REQ_CHANNEL = int(environ.get('INDEX_REQ_CHANNEL', str(LOG_CHANNEL)))
+
+LOG_STR = "Current Cusomized Configurations are:-\n"
+LOG_STR += ("IMDB Results are enabled, Bot will be showing imdb details for you queries.\n" if IMDB else "IMBD Results are disabled.\n")
+LOG_STR += ("P_TTI_SHOW_OFF found , Users will be redirected to send /start to Bot PM instead of sending file file directly\n" if P_TTI_SHOW_OFF else "P_TTI_SHOW_OFF is disabled files will be send in PM, instead of sending start.\n")
+LOG_STR += ("SINGLE_BUTTON is Found, filename and files size will be shown in a single button instead of two seperate buttons\n" if SINGLE_BUTTON else "SINGLE_BUTTON is disabled , filename and file_sixe will be shown as diffrent buttons\n")
+LOG_STR += (f"CUSTOM_FILE_CAPTION enabled with value {CUSTOM_FILE_CAPTION}, your files will be send along with this customized caption.\n" if CUSTOM_FILE_CAPTION else "No CUSTOM_FILE_CAPTION Found, Default captions of file will be used.\n")
+LOG_STR += ("Long IMDB storyline enabled." if LONG_IMDB_DESCRIPTION else "LONG_IMDB_DESCRIPTION is disabled , Plot will be shorter.\n")
+LOG_STR += ("Spell Check Mode Is Enabled, bot will be suggesting related movies if movie not found\n" if SPELL_CHECK_REPLY else "SPELL_CHECK_REPLY Mode disabled\n")
+LOG_STR += (f"MAX_LIST_ELM Found, long list will be shortened to first {MAX_LIST_ELM} elements\n" if MAX_LIST_ELM else "Full List of casts and crew will be shown in imdb template, restrict them by adding a value to MAX_LIST_ELM\n")
+LOG_STR += f"Your Currect IMDB template is {IMDB_TEMPLATE}"
